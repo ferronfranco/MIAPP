3f85df96edcc2de08b5755bf3b3c4e7432deec36
ferronfranco <ferronfranco291@gmail.com>
2023-11-23 10:18:46 -0300

    appp recet

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  return ( 
     <View style={styles.container}>
       <Text>Open !</Text>
       <StatusBar style="auto" />
